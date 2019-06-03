53defdc6b1969c8258d084a275b8ad07ec8a802d
boruch d <dubywider@gmail.com>
2019-06-03 17:50:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,23 @@
- class Song
- 
-  attr_accessor :name,
-  
- 
-  def initialize(name)
+class Song 
+  attr_accessor :name, :artist 
+
+  @@all = []
+
+  def initialize(name, artist=nil)
     @name = name
-     @@all = []
-  @@all  << name 
+    @artist = artist
+    @@all << self
   end
 
-  def all 
-   @@all 
-  end 
- def artist_name
-    self.artist.name
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
   end
- 
+
+  def self.all
+    @@all
+  end
 end
