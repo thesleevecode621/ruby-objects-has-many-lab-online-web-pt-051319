fa1d49ef4e48c605e8ff8fdcde8268effc2821ea
boruch d <dubywider@gmail.com>
2019-06-03 19:13:12 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  attr_accessor :author, :title 
+@@all = []
+   def initialize(title)
+    @title = title
+    @author = author
+    @@all << self  
+  end
+
+   def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
+  end
+ def self.all
+    @@all
+  end
+ end 
