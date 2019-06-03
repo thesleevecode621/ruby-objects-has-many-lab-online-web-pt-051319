04e818502f408f19b675cc61d6388adfe7e3c4c0
boruch d <dubywider@gmail.com>
2019-06-03 17:53:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,7 +11,11 @@
     @songs << song
     song.artist = self
   end
- 
+ def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+  end
   def songs
     @songs
   end
