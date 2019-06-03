274f06ec570c2ca05070914a60ee63ac3f8c516e
boruch d <dubywider@gmail.com>
2019-06-03 19:03:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,31 @@
 class Author 
-attr_accessor :name
+  attr_accessor :name
+  @@post_count = 0
 
-  def initialize(name)
+   def initialize(name)
     @name = name
     @posts = []
   end
-  def posts
+
+   def posts
     @posts
   end
-  def add_post(posts)
-    @posts << posts 
-    posts.author = self 
-  end 
-  def add_post_by_title(title)
-    posts = Post.new(title)
-    posts.author = self
-  end 
-  
-end 
+
+   def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+   def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+   def self.post_count
+    @@post_count
+  end
+
+ end 
