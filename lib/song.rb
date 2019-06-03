f2140b60aa57bc11c493fb4d02ff3e6ea484a04c
boruch d <dubywider@gmail.com>
2019-06-03 17:07:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
-class Songs
-  attr_accessor :title 
+ class Songs 
+  attr_accessor :name
  
   def initialize(name)
     @name = name
+    @songs = []
   end
-end
+ 
+  def add_song(song)
+    @songs << song
+  end
+
+end 
