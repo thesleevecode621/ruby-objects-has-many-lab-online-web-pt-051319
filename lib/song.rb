3a4fcc1331fb79084e93e66796290126fa8ffbfc
boruch d <dubywider@gmail.com>
2019-06-03 17:34:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
  class Song
  
   attr_accessor :name,
- 
+ @@all
   def initialize(name)
     @name = name
-   
+   @@all<< name 
   end
+  def all 
+    @@all 
+  end 
  def artist_name
     self.artist.name
   end
